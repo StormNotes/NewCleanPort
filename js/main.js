d5efb8eb8c40839b71370d5d9b7f6c1077361a09
Leonardo Diaz <stormnotesnotes@gmail.com>
2019-03-04 00:32:22 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-// Select DOM Items
+
+
+
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-// Set Initial State Of Menu
+
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
